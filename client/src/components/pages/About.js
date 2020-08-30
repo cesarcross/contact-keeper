7c7fb212cbef84a152f5c7891a04451885a5ba63
Cesar Corregiari <cesarcorregiari@gmail.com>
2020-08-30 18:23:41 -0300

    crud contacts

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p className="my-1">This is a React app for keeping contacts</p>
+      <p className="bg-dark p">This is blabla</p>
+    </div>
+  );
+};
+
+export default About;
